b6a7aafa56a92ccd1ec496d4eca2fe11f28fa7c0
BandagondaSaiSirisha <bsaisirishagoud888@gmail.com>
2023-10-13 18:01:32 +0530

    implemented BMI calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, UntypedFormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bmi-calculator-angular';
+
+  calculatedBMI: any;
+
+  bmiCalculatorGroup = new FormGroup({
+    weight: new UntypedFormControl('', [Validators.required]),
+    height: new UntypedFormControl('', [Validators.required]),
+  })
+
+  calculateBMI() {    
+    let height = (this.bmiCalculatorGroup.get('height')?.value) * 0.01
+    this.calculatedBMI = (this.bmiCalculatorGroup.get('weight')?.value / (height * height)).toFixed(2)
+
+    console.log(this.calculatedBMI);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
